e53e736bf6714e7719c4cc593333712845770464
TyIL TTY7 <q1342893971@gmail.com>
2020-08-12 23:06:14 +0800

    [fix](crawler_requests_session) refresh user-agent

--- crawler_requests_session.py
+++ crawler_requests_session.py
@@ -17,18 +17,19 @@ class Crawler_Requests_Session(object):
         proxies_list: List[Dict[str, str]] = [{'http': '', 'https': ''}],
         sleep_interval_range: Tuple[float, float] = (0.5, 2.0),
     ):
-        self._user_agent = user_agent
+        self.__fua = fua.UserAgent()
+        self._user_agent = user_agent if user_agent != '' else self.__fua.random
         self._proxies_list = proxies_list
         self._sleep_interval_range = sleep_interval_range
-        self.__fua = fua.UserAgent()
         self.refresh_identity()
 
-    def refresh_identity(self):
+    def refresh_identity(self, refresh_user_agent: bool = True):
         self.__session = requests.Session()
+        if refresh_user_agent:
+            self.__session.headers.update({'User-Agent': self.__fua.random})
+        else:
+            self.__session.headers.update({'User-Agent': self._user_agent})
         self.__session.proxies.update(random.choice(self._proxies_list))
-        if self._user_agent == '':
-            self._user_agent == self.__fua.random
-        self.__session.headers.update({'User-Agent': self._user_agent})
 
     def get(self, *args, **kwargs):
         time.sleep(random.uniform(*self._sleep_interval_range))
