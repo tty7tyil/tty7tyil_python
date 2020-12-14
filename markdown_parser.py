bb8c100d277ab13525a59c7a176767f5581730bf
TyIL TTY7 <B90FCF15A76E432D@outlook.com>
2020-12-14 23:22:57 +0800

    [feature] implement serial counting for duplicate toc header

--- markdown_parser.py
+++ markdown_parser.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import dataclasses as dcs
 import datetime as dt
 import re
 from typing import Dict, List, Tuple, Union
 
 from tty7tyil_python import normalize_string as ns
 from tty7tyil_python import tree
+
+
+@dcs.dataclass
+class TOC_Header_Data:
+    level_in_file: int
+    content: str
+    serial: int
 
 
 class MD_Data:
@@ -19,7 +27,8 @@ class MD_Data:
                 None,
             ]
         ] = MD_Data.extract_file_header(md_file_content)
-        self.toc: tree.Tree[str] = MD_Data.extract_toc(md_file_content)
+        self.toc: tree.Tree[TOC_Header_Data] = MD_Data.extract_toc(md_file_content)
+        self.content = md_file_content
 
     @staticmethod
     def extract_file_header(md_file_content: str):
@@ -31,26 +40,45 @@ class MD_Data:
     )
 
     @staticmethod
-    def extract_toc(md_file_content: str) -> tree.Tree[str]:
-        toc_list = MD_Data.__REGEX_TOC_HEADER.findall(md_file_content)
-        return MD_Data.__fill_toc_tree(tree.Tree('Table of Contents'), toc_list)[0]
+    def extract_toc(md_file_content: str) -> tree.Tree[TOC_Header_Data]:
+        raw_toc_list = MD_Data.__REGEX_TOC_HEADER.findall(md_file_content)
+        toc_header_data_list = []
+
+        serial_dict: Dict[str, int] = {}
+        # initialize the serial to 0 for every toc content
+        for e in raw_toc_list:
+            serial_dict[e[1]] = 0
+        # fill the list while counting serial
+        for e in raw_toc_list:
+            toc_header_data_list.append(TOC_Header_Data(len(e[0]), e[1], serial_dict[e[1]]))
+            serial_dict[e[1]] += 1
+
+        return MD_Data.__fill_toc_tree(
+            tree.Tree(TOC_Header_Data(0, 'Table of Contents', 0)),
+            toc_header_data_list,
+        )[0]
 
     @staticmethod
     def __fill_toc_tree(
-        toc_tree: tree.Tree[str],
-        toc_list: List[Tuple[str, str]],
+        toc_tree: tree.Tree[TOC_Header_Data],
+        toc_header_data_list: List[TOC_Header_Data],
+        /,
         start_index: int = 0,
-    ) -> Tuple[tree.Tree[str], int]:
-        toc_level = len(toc_list[start_index][0])
+    ) -> Tuple[tree.Tree[TOC_Header_Data], int]:
+        toc_level = toc_header_data_list[start_index].level_in_file
         i = start_index
         while True:
-            if i == len(toc_list):
+            if i == len(toc_header_data_list):
                 return toc_tree, i
-            elif len(toc_list[i][0]) == toc_level:
-                toc_tree.branches.append(tree.Tree(toc_list[i][1]))
+            elif toc_header_data_list[i].level_in_file == toc_level:
+                toc_tree.branches.append(tree.Tree(toc_header_data_list[i]))
                 i += 1
-            elif toc_level < len(toc_list[i][0]):
-                sub_tree, i = MD_Data.__fill_toc_tree(toc_tree.branches[-1], toc_list, i)
+            elif toc_level < toc_header_data_list[i].level_in_file:
+                sub_tree, i = MD_Data.__fill_toc_tree(
+                    toc_tree.branches[-1],
+                    toc_header_data_list,
+                    i
+                )
                 toc_tree.branches[-1] = sub_tree
             else:
                 return toc_tree, i
